e736bedd019ebd04dc59e8539b029ed4654d6872
Tanguy <t.pincent29@gmail.com>
2019-05-06 15:02:58 +0200

    fonction

--- Modele/insert.php
+++ Modele/insert.php
@@ -22,5 +22,24 @@ function insertArticle($s1,$s2,$s3,$s4){
     return $check;
 }
 
+
+function createMembre()
+{
+    $reponse = $bdd->prepare("INSERT INTO `membre` (`nom`, `prenom`, `mail`, `mdp`, `adresse`,`statut`,`magasin`,`0`, `idTheme`,) 
+    VALUES (:nom,:prenom,:mail,:pass,:adresse,:theme,:grade,:magasin);");
+    $reponse->execute(array(
+        'nom' => $_SESSION['nom'],
+        'prenom' => $_SESSION["prenom"],
+        'mail' => $_SESSION["mail"],
+        'pass' => $_SESSION["pass"],
+        'adresse' => $_SESSION["adresse"],
+        'theme' => $_SESSION["theme"],
+        'grade' => $_SESSION["grade"],
+        'magasin' => $_SESSION["magasin"]
+    ));
+}
+
+
 ?>
+
 
--- Modele/select.php
+++ Modele/select.php
@@ -2,32 +2,80 @@
 include("Modele/connectBDD.php");
 
 // Requete Amine
-function selectCategorie(){
+function selectCategorie()
+{
     $bdd = $GLOBALS["bdd"];
     $reponse = $bdd->query("SELECT * FROM categorie");
     return $reponse;
 }
 
-function selectArticle(){
+function selectArticle()
+{
     $bdd = $GLOBALS["bdd"];
     $reponse = $bdd->query("SELECT * FROM article");
     return $reponse;
 }
 
-// Amine Annia
+// Requete Annia
 
 
-function selectMembre($bdd) {
-	$reponse = $bdd ->prepare('SELECT * FROM membre');
-	$reponse -> execute(null);
+function selectMembre($bdd)
+{
+    $reponse = $bdd->prepare('SELECT * FROM membre');
+    $reponse->execute(null);
 
-	while ($donnees[] = $reponse->fetch()) {
-
-	}
-	return $donnees;
+    while ($donnees[] = $reponse->fetch()) { }
+    return $donnees;
 }
 
 // Requete Tanguy
 
 
+/////// Trouve un Mot de passe  ////
+function mdp()
+{
+    $bdd = $GLOBALS["bdd"];
+    $reponse = $bdd->prepare("SELECT `mdp` FROM `membre` WHERE `mail` LIKE :mail");
+    $reponse->execute(array(
+        'mail' => $_SESSION['mail']
+    ));
+
+
+    while ($donnees = $reponse->fetch()) {
+        $mdp = $donnees['mdp'];
+
+    }
+    return $mdp;
+}
+/////// Trouve un pseudo  ////
+function prenom()
+{
+    $bdd = $GLOBALS["bdd"];
+    $reponse2 = $bdd->prepare("SELECT `prenom` FROM `membre` WHERE `mail` LIKE :mail");
+    $reponse2->execute(array(
+        'mail' => $_SESSION['mail']
+    ));
+
+
+    while ($donnees2 = $reponse2->fetch()) {
+        $_SESSION['prenom'] = $donnees2['prenom'];
+    }
+    return  $_SESSION['prenom'];
+}
+
+/////// Trouve un statut  ////
+function statut()
+{
+    $bdd = $GLOBALS["bdd"];
+    $reponse3 = $bdd->prepare("SELECT `statut` FROM `membre` WHERE `mail` LIKE :mail");
+    $reponse3->execute(array(
+        'mail' => $_SESSION['mail']
+    ));
+
+
+    while ($donnees3 = $reponse3->fetch()) {
+        $grade = $donnees3['statut'];
+    }
+    return $grade;
+}
 ?>
