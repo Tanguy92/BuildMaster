88cd279db1cd39fe8e9654e9f61327d9610dbd58
Ihsane <ihsane91170@hotmail.fr>
2019-07-03 11:31:53 +0200

    categories pretes

--- Controleur/Admin/categorie.php
+++ Controleur/Admin/categorie.php
@@ -4,7 +4,18 @@ include("View/Template/templateTopAdmin.html");
 include("Modele/select.php");
 include("Modele/insert.php");
 include("Modele/delete.php");
-include("View/Admin/categorie.php");
+
+if (isset($_POST['ajoutCategorie'])) {
+    $nom = $_POST['ajoutCategorie'];
+    $ajoutCategorie = insertCategorie($nom);
+   
+}
+
+if (isset($_POST['supprCategorie'])){
+    $idCategorie = $_POST['supprCategorie'];
+    updateDeleteCategorie($idCategorie);
+    $supprCategorie = deleteCategorie($idCategorie);
+}
 
 $reponse = selectAllCategorie();
 while ($donnees = $reponse->fetch()) {
@@ -12,19 +23,5 @@ while ($donnees = $reponse->fetch()) {
     $nom = $donnees['nom'];
 }
 
-if (isset($_POST['ajoutCategorie'])) {
-    $nom = $_POST['ajoutCategorie'];
-    $ajoutCategorie = insertCategorie($nom);
-    header('Location: index.php?page=categorie');
-    while ($donnees = $reponse->fetch()) {
-    }
-}
-
-if (isset($_POST['supprCategorie'])){
-    $idCategorie = $_POST['supprCategorie'];
-    updateDeleteCategorie($idCategorie);
-    $supprCategorie = deleteCategorie($idCategorie);
-    header('Location: index.php?page=categorie');
-}
-
+include("View/Admin/categorie.php");
 ?>
